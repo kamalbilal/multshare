2bd0359a5dc1b330954491a81452b08104d61593
kamalbilal <48208756+kamalbilal@users.noreply.github.com>
2021-08-03 07:18:28 -0700

    Update app.js

--- app.js
+++ app.js
@@ -62,5 +62,8 @@ app.use("/file", filedownloaderRoute);
 
 /////////////////
 //Server Listen
-//const port = 3000;
-app.listen();
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
